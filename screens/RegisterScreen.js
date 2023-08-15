7be4d44df2afd9b146f6326c46e222a4a548bb99
Fernanda <mangorifernanda@gmail.com>
2023-08-15 19:42:33 +0800

    scroll register

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -6,7 +6,8 @@ import {
   Image,
   KeyboardAvoidingView,
   Pressable,
-  TextInput
+  TextInput,
+  ScrollView
 } from 'react-native'
 import React, { useState } from 'react'
 import { amazon } from '../assets'
@@ -21,119 +22,120 @@ const RegisterScreen = () => {
 
   return (
     <SafeAreaView style={styles.safeArea}>
-      <View>
-        <Image
-          style={styles.images}
-          source={amazon}
-        />
-      </View>
-
-      <KeyboardAvoidingView>
-        <View style={styles.view1}>
-          <Text style={styles.textLogin}>Register your Account</Text>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View>
+          <Image
+            style={styles.images}
+            source={amazon}
+          />
         </View>
 
-        <View style={{ marginTop: 70 }}>
-
-          <View style={styles.view2}>
-            <Ionicons
-              style={styles.materialIcon}
-              name="person-sharp" size={24} color="black" />
-
-            <TextInput
-              value={name}
-              onChangeText={(text) => setName(text)}
-              style={styles.textInputEmail}
-              placeholder={"enter your Name"}
-            />
-
+        <KeyboardAvoidingView>
+          <View style={styles.view1}>
+            <Text style={styles.textLogin}>Register your Account</Text>
           </View>
-        </View>
 
-        <View style={{ marginTop: 10 }}>
-          <View style={styles.view2}>
-            <MaterialIcons
-              style={styles.materialIcon}
-              name="email" size={24} color="black" />
+          <View style={{ marginTop: 70 }}>
 
-            <TextInput
-              value={email}
-              onChangeText={(text) => setEmail(text)}
-              style={styles.textInputEmail}
-              placeholder={"enter your E-mail"}
-            />
+            <View style={styles.view2}>
+              <Ionicons
+                style={styles.materialIcon}
+                name="person-sharp" size={24} color="black" />
 
+              <TextInput
+                value={name}
+                onChangeText={(text) => setName(text)}
+                style={styles.textInputEmail}
+                placeholder={"enter your Name"}
+              />
+
+            </View>
           </View>
-        </View>
 
+          <View style={{ marginTop: 10 }}>
+            <View style={styles.view2}>
+              <MaterialIcons
+                style={styles.materialIcon}
+                name="email" size={24} color="black" />
 
-        <View style={{ marginTop: 10 }}>
-          <View style={styles.view2}>
-            <AntDesign
-              style={styles.materialIcon}
-              name="lock1" size={24} color="black" />
+              <TextInput
+                value={email}
+                onChangeText={(text) => setEmail(text)}
+                style={styles.textInputEmail}
+                placeholder={"enter your E-mail"}
+              />
 
-            <TextInput
-              value={password}
-              onChangeText={(text) => setPassword(text)}
-              secureTextEntry={true}
-              style={styles.textInputPassword}
-              placeholder={"enter your Password"}
-            />
+            </View>
           </View>
-        </View>
 
-        <View style={{ marginTop: 10 }}>
-          <View style={styles.view2}>
-            <AntDesign
-              style={styles.materialIcon}
-              name="lock1" size={24} color="black" />
 
-            <TextInput
-              value={password}
-              onChangeText={(text) => setPassword(text)}
-              secureTextEntry={true}
-              style={styles.textInputPassword}
-              placeholder={"enter your Password Again"}
-            />
+          <View style={{ marginTop: 10 }}>
+            <View style={styles.view2}>
+              <AntDesign
+                style={styles.materialIcon}
+                name="lock1" size={24} color="black" />
+
+              <TextInput
+                value={password}
+                onChangeText={(text) => setPassword(text)}
+                secureTextEntry={true}
+                style={styles.textInputPassword}
+                placeholder={"enter your Password"}
+              />
+            </View>
           </View>
-        </View>
 
-        <View style={{
-          marginTop: 12,
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between"
-        }}>
-          <Text>Keep me logged in</Text>
+          <View style={{ marginTop: 10 }}>
+            <View style={styles.view2}>
+              <AntDesign
+                style={styles.materialIcon}
+                name="lock1" size={24} color="black" />
 
-          <Text style={{ color: "#007FFF", fontWeight: "500" }}> Forgot Password</Text>
-        </View>
-        <View style={{ marginTop: 70 }} />
+              <TextInput
+                value={password}
+                onChangeText={(text) => setPassword(text)}
+                secureTextEntry={true}
+                style={styles.textInputPassword}
+                placeholder={"enter your Password Again"}
+              />
+            </View>
+          </View>
 
-        <Pressable style={{
-          width: 200,
-          backgroundColor: "#FEBE10",
-          borderRadius: 6,
-          marginLeft: "auto",
-          marginRight: "auto",
-          padding: 15
-        }}>
-          <Text>
-            Login
-          </Text>
-        </Pressable>
+          <View style={{
+            marginTop: 12,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between"
+          }}>
+            <Text>Keep me logged in</Text>
 
-        <Pressable onPress={() => navigation.goBack()}>
-          <Text style={{
-            textAlign: "center",
-            color: "grey",
-            fontSize: 16
-          }}
-          >Already have an account ? Sign In</Text>
-        </Pressable>
-      </KeyboardAvoidingView>
+            <Text style={{ color: "#007FFF", fontWeight: "500" }}> Forgot Password</Text>
+          </View>
+          <View style={{ marginTop: 70 }} />
 
+          <Pressable style={{
+            width: 200,
+            backgroundColor: "#FEBE10",
+            borderRadius: 6,
+            marginLeft: "auto",
+            marginRight: "auto",
+            padding: 15
+          }}>
+            <Text>
+              Login
+            </Text>
+          </Pressable>
+
+          <Pressable onPress={() => navigation.goBack()}>
+            <Text style={{
+              textAlign: "center",
+              color: "grey",
+              fontSize: 16
+            }}
+            >Already have an account ? Sign In</Text>
+          </Pressable>
+        </KeyboardAvoidingView>
+      </ScrollView>
     </SafeAreaView>
   )
 }
