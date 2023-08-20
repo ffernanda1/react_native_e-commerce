93a1813eda1ad30eedaf236534c5d4c8ca3e3ea7
Fernanda <mangorifernanda@gmail.com>
2023-08-20 21:42:57 +0800

    verification

--- api/index.js
+++ api/index.js
@@ -28,3 +28,83 @@ mongoose.connect("mongodb+srv://mangorifernanda:ffrostbite@cluster0.g1rmixt.mong
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+const User = require("./models/user");
+const Order = require("./models/order");
+
+//function to send Verification Email to the user
+const sendVerificationEmail = async (email, verificationToken) => {
+    //create a nodemailer transport 
+    const transporter = nodemailer.createTransport({
+        //configure the email service
+
+    });
+
+    const mailOptions = {
+        from: "amazon.com",
+        to: email,
+        subject: "Email Verification",
+        text: `Please click the following link to verify your email : http://localhost:8000/verify/${verificationToken}`
+    };
+
+    //send email
+    try {
+        await transporter.sendMail(mailOptions);
+    } catch (error) {
+        console.log({ message: 'error sending verification email', error })
+
+    }
+}
+
+//endpoint to register in the app
+app.post("/register", async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "Email already registered" })
+        }
+
+        const newUser = new User({ firstName, lastName, email, password })
+        //generate and store verification
+        newUser.verificationToken = crypto.randomBytes(20).toString("hex");
+
+        //save the user to database
+        await newUser.save();
+
+        //send Verification email to the user
+
+        sendVerificationEmail(newUser.email, newUser.verificationToken);
+
+
+    } catch (error) {
+
+        console.log("Error registering", error)
+        res.status(500).json({ message: "registration failed" })
+    }
+});
+
+//endpoint verify email
+
+app.get('/verify/:token', async (req, res) => {
+    try {
+        const token = req.params.token;
+
+        //Find the user with given verification token
+
+        const user = await User.findOne({ verificationToken: token });
+        if (!user) {
+            return res.status(404).json({ message: "Invalid verification token" });
+        };
+        //Mark the user as verified
+        user.verified = true;
+        user.verificationToken = undefined;
+
+        await user.save();
+
+        res.status(200).json({ message: "email Verified successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "email verification failed" })
+    }
+});
--- api/models/order.js
+++ api/models/order.js
@@ -56,5 +56,17 @@ const orderSchema = new mongoose.Schema({
             type: String,
             required: true,
         },
+    },
+    paymentMethod: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
     }
-})
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
