5e1c21895fd3b82871d867d2033f5e7362b5aae2
Fernanda <mangorifernanda@gmail.com>
2023-09-02 20:42:47 +0800

    import home

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  ScrollView,
+  Pressable,
+  TextInput,
+  Image,
+} from "react-native";
+import React, { useState, useEffect, useCallback, useContext } from "react";
+import { Feather } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { SliderBox } from "react-native-image-slider-box";
+import axios from "axios";
+import ProductItem from "../components/ProductItem";
+import DropDownPicker from "react-native-dropdown-picker";
+import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import { BottomModal, SlideAnimation, ModalContent } from "react-native-modals";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { UserType } from "../UserContext";
+import jwt_decode from "jwt-decode";
 
 const HomeScreen = () => {
   return (
